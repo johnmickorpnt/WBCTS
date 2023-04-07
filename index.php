543a803d82e1838a96874b1aeef96502508bdc1a
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2023-04-07 23:33:42 +0800

    Added files

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
 
-if ($_SESSION['email']) {
+if (isset($_SESSION['email'])) {
 	echo "Welcome bitch" . $_SESSION["email"];
-}else{
+} else {
 	header("");
 }
 
@@ -12,15 +12,18 @@ if ($_SESSION['email']) {
 
 <!DOCTYPE html>
 <html>
+
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Blotter Website</title>
 </head>
+
 <body>
 	<h1>Sample Page</h1>
-		<a href="logout.php">LOGOUT</a> 
+	<a href="logout.php">LOGOUT</a>
 
-		<p>Hello User, please give me money</p>
+	<p>Hello User, please give me money</p>
 </body>
+
 </html>
--- registration.php
+++ registration.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <?php
 session_start();
 	//ako ay isang comment
@@ -23,27 +24,30 @@ session_start();
 	}
 ?>
 
+=======
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Registration form</title>
 
-<link rel="stylesheet"  href="css/style.css">
-<style>
+<head>
+  <title>Registration form</title>
 
-@import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
-*{
-  margin: 0;
-  padding: 0;
-  outline: none;
-  box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
-}
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
+  <link rel="stylesheet" href="css/style.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
 
-</style>
+    * {
+      margin: 0;
+      padding: 0;
+      outline: none;
+      box-sizing: border-box;
+      font-family: 'Poppins', sans-serif;
+    }
+
+    body {
+      font-family: Arial, Helvetica, sans-serif;
+    }
+  </style>
 </head>
 
 <body>
@@ -51,51 +55,47 @@ body {
   <div class="container">
     <center>
       <h1>REGISTRATION</h1>
-        <p>Please fill up the form to have an account</p>
+      <p>Please fill up the form to have an account</p>
     </center>
     <hr>
     <form name="RegistrationForm" method="post" onsubmit="return ValidateForms()">
-    <div class="field">
-        
-       <label for="username"><b>Username:</b></label><br>
+      <div class="field">
+
+        <label for="username"><b>Username:</b></label><br>
         <input type="text" placeholder="Enter Username" name="username" id="username">
-    </div>
+      </div>
 
-    <div class="field">
+      <div class="field">
 
-    <label for="password"><b>Password:</b></label><br>
-    <input type="password" placeholder="Enter Password" name="password" id="password" >
-    </div>
+        <label for="password"><b>Password:</b></label><br>
+        <input type="password" placeholder="Enter Password" name="password" id="password">
+      </div>
 
-    <label for="email"><b>Email:</b></label><br>
-    <input type="email" placeholder="Enter Email" name="email" id="email" >
+      <label for="email"><b>Email:</b></label><br>
+      <input type="email" placeholder="Enter Email" name="email" id="email">
 
-    <hr>
-     <p>Already have an account? <a href="login.php">Sign in here</a>.</p>
-    <div class="registbtn">
-    <button type="onsubmit" onclick="ValidateForms()"/>
-    <a href="home.html">Register</a></button>
-  </div>
-  </form>
-<script type="text/javascript">
+      <hr>
+      <p>Already have an account? <a href="login.php">Sign in here</a>.</p>
+      <div class="registbtn">
+        <button type="onsubmit" onclick="ValidateForms()" />
+        <a href="home.html">Register</a></button>
+      </div>
+    </form>
+    <script type="text/javascript">
+      function ValidateForms() {
+        if (document.getElementById("username").value == "") {
+          alert("Username cannot be blank");
 
-function ValidateForms()
-{
-  if(document.getElementById("username").value=="")
-  {
-    alert("Username cannot be blank");
- 
-  }
-  if(document.getElementById("password").value=="")
-  {
-    alert("Password cannot be blank");
-  
-  }
-  if(document.getElementById("email").value=="")
-  {
-    alert("Email cannot be blank"); 
-  } 
-}
-</script>
+        }
+        if (document.getElementById("password").value == "") {
+          alert("Password cannot be blank");
+
+        }
+        if (document.getElementById("email").value == "") {
+          alert("Email cannot be blank");
+        }
+      }
+    </script>
 </body>
+
 </html>
