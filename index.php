528d5749496ad7bcb250cb5b204922aeba04f974
Maykelzzz <100915382+Maykelzzz@users.noreply.github.com>
2023-04-09 15:53:53 +0800

    Created new style for index

--- index.php
+++ index.php
@@ -11,19 +11,37 @@ if (isset($_SESSION['email'])) {
 ?>
 
 <!DOCTYPE html>
+<html lang="en">
 <html>
 
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Blotter Website</title>
+
+	<link rel="stylesheet" href="css/style1.css">
 </head>
 
-<body>
-	<h1>Sample Page</h1>
-	<a href="logout.php">LOGOUT</a>
 
-	<p>Hello User, please give me money</p>
+<body>
+	<header>
+		<a href="#" class="logo"><img src="image/wbctsLogo.png"></a>
+			<nav class="navbar">
+				<ul>
+					<li><a href="#">Home</a></li>
+					<li><a href="#">Settlements</a></li>
+					<li><a href="#">Blotter Records</a></li>
+					<li><a href="#">QR Code Tracking</a></li>
+					<li><a href="#">Profile</a>
+						<ul>
+							<li><a href="#">Personal Information</li></a>
+							<li><a href="#">Log out</li></a>
+						</ul>
+					</li>
+				</ul>
+
+			</nav>
+	</header>
 </body>
 
 </html>
