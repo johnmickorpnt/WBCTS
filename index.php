0d95a71cfc7bfc1a56497ecae724d714a319fa7f
Maykelzzz <100915382+Maykelzzz@users.noreply.github.com>
2023-04-09 21:26:05 +0800

    Unfinished

--- index.php
+++ index.php
@@ -26,16 +26,18 @@ if (isset($_SESSION['email'])) {
 <body>
 	<header>
 		<a href="#" class="logo"><img src="image/wbctsLogo.png"></a>
+			<input type="checkbox" id="menu-bar">
+			<label for="menu-bar">Menu</label>
 			<nav class="navbar">
 				<ul>
-					<li><a href="#">Home</a></li>
-					<li><a href="#">Settlements</a></li>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="OnSettlements.php">Settlements</a></li>
 					<li><a href="#">Blotter Records</a></li>
 					<li><a href="#">QR Code Tracking</a></li>
 					<li><a href="#">Profile</a>
 						<ul>
 							<li><a href="#">Personal Information</li></a>
-							<li><a href="#">Log out</li></a>
+							<li><a href="logout.php">Log out</li></a>
 						</ul>
 					</li>
 				</ul>
