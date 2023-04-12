ef8942c6726a70d65aee8c951a808d629dacb0f8
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2023-04-12 19:19:48 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,31 +1,3 @@
-<<<<<<< Updated upstream
-<?php
-session_start();
-	//ako ay isang comment
-	include("connection.php");
-	include("function.php");
-
-	if($_SERVER['REQUEST_METHOD'] == "POST")
-	{
-		$user_name = $_POST['user_name'];
-		$password = $_POST['password'];
-
-		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
-		{
-			$query = "insert into users (user_id,user_name,password) values ('$user_id','$user_name','$password')";
-			mysqli_query($query);
-
-			header("Location: login.php");
-			die;
-
-		}else{
-			echo "Please enter valid information!";
-		}
-	}
-?>
-
-=======
->>>>>>> Stashed changes
 <!DOCTYPE html>
 <html>
 
