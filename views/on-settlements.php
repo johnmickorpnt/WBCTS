96210e614afd4f909deeaa53d9c788234e481642
Maykelzzz <100915382+Maykelzzz@users.noreply.github.com>
2023-05-03 19:49:42 +0800

    Table - Idk

--- views/on-settlements.php
+++ views/on-settlements.php
@@ -1,111 +1,56 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet"  href="assets/css/style.css">
+    </head>
+</html>
 <?php
 require("../templates/template-functions.php");
-
 $title = "On Settlements";
 $content = <<<CONTENT
-<table id="example" class="table table-striped" style="width:100%; margin-top:10rem; text-align:center">
+
+
+
+<table class="table-sortable">
         <thead>
             <tr>
+                <th>Rank</th>
                 <th>Name</th>
-                <th>Job</th>
-                <th>Office</th>
                 <th>Age</th>
-                <th>Start date</th>
+                <th>Occupation</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Antipolo</td>
-                <td>61</td>
-                <td>2011-04-25</td>
+                <td>1</td>
+                <td>Dom</td>
+                <td>35</td>
+                <td>Web Developer</td>
             </tr>
             <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Antipolo</td>
-                <td>63</td>
-                <td>2011-07-25</td>
+                <td>2</td>
+                <td>Rebecca</td>
+                <td>29</td>
+                <td>Teacher</td>
             </tr>
             <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>Antipolo</td>
-                <td>66</td>
-                <td>2009-01-12</td>
-            </tr>
-            <tr>
-                <td>Cedric Kelly</td>
-                <td>Senior Javascript Developer</td>
-                <td>Antipolo</td>
-                <td>22</td>
-                <td>2012-03-29</td>
-            </tr>
-            <tr>
-                <td>Airi Satou</td>
-                <td>Accountant</td>
-                <td>Antipolo</td>
-                <td>33</td>
-                <td>2008-11-28</td>
-            </tr>
-            <tr>
-                <td>Brielle Williamson</td>
-                <td>Integration Specialist</td>
-                <td>Antipolo</td>
-                <td>61</td>
-                <td>2012-12-02</td>
-            </tr>
-            <tr>
-                <td>Herrod Chandler</td>
-                <td>Sales Assistant</td>
-                <td>Antipolo</td>
-                <td>59</td>
-                <td>2012-08-06</td>
-            </tr>
-            <tr>
-                <td>Rhona Davidson</td>
-                <td>Integration Specialist</td>
-                <td>Antipolo</td>
-                <td>55</td>
-                <td>2010-10-14</td>
-            </tr>
-            <tr>
-                <td>Colleen Hurst</td>
-                <td>Javascript Developer</td>
-                <td>Antipolo</td>
-                <td>39</td>
-                <td>2009-09-15</td>
-            </tr>
-            <tr>
-                <td>Sonya Frost</td>
-                <td>Software Engineer</td>
-                <td>Antipolo</td>
-                <td>23</td>
-                <td>2008-12-13</td>
-            </tr>
-            <tr>
-                <td>Jena Gaines</td>
-                <td>Office Manager</td>
-                <td>Antipolo</td>
+                <td>3</td>
+                <td>John</td>
                 <td>30</td>
-                <td>2008-12-19</td>
+                <td>Civil Engineer</td>
             </tr>
             <tr>
-                <td>Quinn Flynn</td>
-                <td>Support Lead</td>
-                <td>Antipolo</td>
-                <td>22</td>
-                <td>2013-03-03</td>
+                <td>4</td>
+                <td>Andre</td>
+                <td>20</td>
+                <td>Dentist</td>
             </tr>
-            <tr>
-                <td>Charde Marshall</td>
-                <td>Regional Director</td>
-                <td>Antipolo</td>
-                <td>36</td>
-                <td>2008-10-16</td>
-            </tr>
+        </tbody>
     </table>
+
+    <script src="./src/tablesort.js"></script>
 CONTENT;
 ?>
 
