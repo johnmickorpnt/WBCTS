8277a61bf75cc674539e4ef9c4c00e0d8163b447
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2023-05-22 04:18:34 +0800

    Update wbcts.sql

--- extra/wbcts.sql
+++ extra/wbcts.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 13, 2023 at 06:44 AM
+-- Generation Time: May 21, 2023 at 10:17 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -54,8 +54,8 @@ INSERT INTO `admin_roles` (`id`, `role`, `permission_create`, `permission_read`,
 
 CREATE TABLE `admin_users` (
   `id` int(11) NOT NULL,
-  `firstname_name` varchar(255) NOT NULL,
-  `last_name` varchar(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
   `role` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL DEFAULT current_timestamp(),
@@ -67,8 +67,9 @@ CREATE TABLE `admin_users` (
 -- Dumping data for table `admin_users`
 --
 
-INSERT INTO `admin_users` (`id`, `firstname_name`, `last_name`, `role`, `username`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'john', 'doe', 1, 'admin', 'admin', '2023-05-12 14:06:23', '2023-05-12 14:06:23');
+INSERT INTO `admin_users` (`id`, `firstname`, `lastname`, `role`, `username`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'john', 'Micko', 1, 'admin', 'admin', '2023-05-12 14:06:23', '2023-05-12 14:06:23'),
+(33, 'JOhn Micko', 'Rapanot', 2, 'pewdie852', 'admin', '2023-05-22 01:50:39', '2023-05-22 01:50:39');
 
 -- --------------------------------------------------------
 
@@ -86,17 +87,20 @@ CREATE TABLE `blotter_records` (
   `incident_type` varchar(255) NOT NULL,
   `blotter_status` varchar(255) DEFAULT 'pending',
   `investigating_officer` varchar(255) DEFAULT NULL,
-  `remarks` varchar(255) NOT NULL
+  `remarks` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `blotter_records`
 --
 
-INSERT INTO `blotter_records` (`id`, `complainant_id`, `respondent_name`, `respondent_address`, `incident_location`, `incident_details`, `incident_type`, `blotter_status`, `investigating_officer`, `remarks`) VALUES
-(1, 17, 'Jao Juan', 'Angono Mahabang Parang', 'Antipolo Rizal', 'Sinuntok ako sa mukha par tapos kinuha wallet ko.', 'Theft', 'pending', 'Michael Zapanta', 'Hulihin niyo pls, kinuwa niya pitaka q huhu.'),
-(2, 17, 'Mark', 'Ministop', 'Ministop', 'Nagpabook si \"kuya\".', 'Staffa', 'pending', 'Michael', 'hahaha kuya ka pala eh'),
-(3, 17, 'fasf', 'asfasf', 'hash', 'asha', 'Verbal Abuse', NULL, NULL, 'hashsash');
+INSERT INTO `blotter_records` (`id`, `complainant_id`, `respondent_name`, `respondent_address`, `incident_location`, `incident_details`, `incident_type`, `blotter_status`, `investigating_officer`, `remarks`, `created_at`, `updated_at`) VALUES
+(1, 17, 'Jao Juan', 'Angono Mahabang Parang', 'Antipolo Rizal', 'Sinuntok ako sa mukha par tapos kinuha wallet ko.', 'Theft', 'pending', 'Michael Zapanta', 'Hulihin niyo pls, kinuwa niya pitaka q huhu.', '2023-05-21 08:35:18', '2023-05-21 08:35:18'),
+(6, 17, 'fasf', 'asfasf', 'das', 'asd', 'asd', 'asd', 'asd', 'nothing', '2023-05-21 23:40:57', '2023-05-21 23:40:57'),
+(7, 17, 'ako lang to', 'sheeesh', 'das', 'dasd', 'asf', 'asf', 'asd', 'asf', '2023-05-22 03:52:52', '2023-05-22 03:52:52'),
+(11, 17, 'fasf', 'Dito lang', 'das', 'dasd', 'asd', 'das', 'dasd', 'asdda', '2023-05-22 04:13:39', '2023-05-22 04:13:39');
 
 -- --------------------------------------------------------
 
@@ -121,7 +125,10 @@ CREATE TABLE `settlements` (
 
 INSERT INTO `settlements` (`id`, `blotter_id`, `resolution`, `settlement_details`, `settled_by`, `remarks`, `date_settled`, `updated_at`) VALUES
 (1, 1, 'yes', 'yeye', '', '', '2023-05-10 15:59:16', '2023-05-10 15:59:16'),
-(2, 3, 'gsa', 'ha', 'hsa', 'hsa', '2023-05-10 15:59:16', '2023-05-10 15:59:16');
+(3, 6, 'haha', 'haha', 'haha', 'asd', '0000-00-00 00:00:00', '2023-05-22 03:41:10'),
+(4, 6, 'yes', 'yeye', '', '', '2023-05-10 15:59:16', '2023-05-22 03:45:49'),
+(5, 1, 'haha', 'haha', 'haha', 'asdfthftghfth', '0000-00-00 00:00:00', '2023-05-22 03:45:58'),
+(8, 6, 'haha', 'hahaururdturu', 'haha', 'fasf', '0000-00-00 00:00:00', '2023-05-22 03:53:42');
 
 -- --------------------------------------------------------
 
@@ -146,8 +153,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `contact_number`, `address`, `password`, `created_at`, `updated`) VALUES
-(17, 'JOhn Micko', 'Rapanot', 'johnmickooo28@gmail.com', '09194282431', '17c Daffodil St. Grandvalley Phase 3, Barangay Mahabang Parang Angono Rizal', '$2y$10$z/swtY3bczDKsUXfQ/AxAuNoE325bMehTicavWINaRzL40DS5QZXm', '2023-04-13 02:23:26', '2023-04-13 02:23:26'),
-(18, 'JOhn Micko', 'Rapanot', 'johnmickorapanot@yahoo.com', '09194282431', '17c Daffodil St. Grandvalley Phase 3, Barangay Mahabang Parang Angono Rizal', '$2y$10$fnkk.ufQoes1AHX9RWqbPuaNtVKG8LWq.EZin0aS0GRcChNJuVNyK', '2023-04-13 10:57:35', '2023-04-13 10:57:35');
+(17, 'John Micko', 'Rapanot', 'johnmickooo28@gmail.com', '09194282431', '17c Daffodil St. Grandvalley Phase 3, Barangay Mahabang Parang Angono Rizal', '$2y$10$z/swtY3bczDKsUXfQ/AxAuNoE325bMehTicavWINaRzL40DS5QZXm', '2023-04-13 02:23:26', '2023-04-13 02:23:26'),
+(18, 'JOhn Micko', 'Rapanot', 'johnmickorapanot@yahoo.com', '09194282431', '17c Daffodil St. Grandvalley Phase 3, Barangay Mahabang Parang Angono Rizal', '$2y$10$fnkk.ufQoes1AHX9RWqbPuaNtVKG8LWq.EZin0aS0GRcChNJuVNyK', '2023-04-13 10:57:35', '2023-04-13 10:57:35'),
+(23, 'John Micko', 'Rapanot', 'johnmickooo28x@gmail.com', '09194282431', '17c Daffodil St. Grandvalley Phase 3, Barangay Mahabang Parang Angono Rizal', 'Siah311_project!', '2023-05-22 00:26:34', '2023-05-22 00:26:34');
 
 --
 -- Indexes for dumped tables
@@ -201,25 +209,25 @@ ALTER TABLE `admin_roles`
 -- AUTO_INCREMENT for table `admin_users`
 --
 ALTER TABLE `admin_users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- AUTO_INCREMENT for table `blotter_records`
 --
 ALTER TABLE `blotter_records`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `settlements`
 --
 ALTER TABLE `settlements`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
