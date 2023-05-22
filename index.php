3f825bd707233c0d95d12757ffced7fa8afd208a
johnmickorpnt <69314516+johnmickorpnt@users.noreply.github.com>
2023-05-22 21:30:25 +0800

    CSS Fixes

--- index.php
+++ index.php
@@ -4,16 +4,12 @@ require("templates/template-functions.php");
 
 $title = "Blotter Website";
 $content = <<<CONTENT
-	<div class="row">
-		<div class="col-1" style="position:relative">
-			<img src="{$img('Justice.jpg')}" alt="Justice" style="width:100%;">
-			<div style="position:absolute; top:50%; left:10%">
-				<h2>Blotter Website <br>Antipolo City</h2>
-				<h3>If it matter, file a blotter </h3>
-				<p>Browse any blotter records at the Records tab </p>
-			</div>
+		<img src="{$img('Justice.jpg')}" alt="Justice" style="width:100%;">
+		<div style="position:absolute; top:50%; left:10%">
+			<h2>Blotter Website <br>Antipolo City</h2>
+			<h3>If it matter, file a blotter </h3>
+			<p>Browse any blotter records at the Records tab </p>
 		</div>
-	</div>
 CONTENT;
 ?>
 <?php include 'templates/default.php'; ?>
